3bb99bbf404162a9cba6303a68ff427240ba9924
Desli <desli@example.com>
2017-07-25 12:41:55 +0200

    register tests

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 var pg = require('pg');
 
 
-var conString = "postgres://postgres:123456789@localhost/testdb";
+var conString = "postgres://postgres:123456789@localhost:5432/testdb";
 
 /* GET about page. */
 router.get('/register', function(req, res) {
@@ -22,6 +23,24 @@ router.get('/register', function(req, res) {
     });
 });
 
-
+router.post('/register/addstudent', function(req, res) {
+    var student = {
+      name: req.body.name,
+      lName: req.body.lName,
+      email: req.body.email,
+      pass1: req.body.pass1,
+      country: req.body.country
+    };
+    console.log(student);
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('INSERT INTO public.user (firstName, lastName, email, password, role, location) VALUES ($1, $2, $3, $4, $5, $6)', [student.name, student.lName, student.email, student.pass1, 'student', student.country]);
+        done();
+        res.redirect("/about");
+        console.log(result.rows);
+    });
+});
 
 module.exports = router;
