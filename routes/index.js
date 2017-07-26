c13c8d116de06915d699a69b54d6a77eb4b70f48
Desli <desli@example.com>
2017-07-26 17:08:22 +0200

    Take courses from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+
+
+var conString = "postgres://postgres:123456789@localhost/youneed";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * from public.course', function (err, result) {
+            if (err) {
+                return console.error('error runing query', err);
+            }
+            res.render('register', {courses: result.rows, title: 'Register'});
+            console.log(result.rows);
+            done();
+        });
+    });
 });
 
 module.exports = router;
