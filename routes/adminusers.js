ed53fd0ec54ddbee7bb8c5957cacfd93b0852a17
Desli <desli@example.com>
2017-07-26 10:22:32 +0200

    Merge branch 'master' of https://github.com/forisaja/ictyouneed
    # Conflicts:
    #	routes/adminusers.js
    #	views/adminusers.ejs

--- routes/adminusers.js
+++ routes/adminusers.js
@@ -7,7 +7,7 @@ var pg = require('pg');
 //     res.render('adminusers', {title: 'Admin Users'});
 // });
 
-var conString = "postgres://fori:123456789@localhost/youneed";
+var conString = "postgres://postgres:123456789@localhost/testdb";
 
 router.get('/adminusers', function(req, res) {
     pg.connect(conString, function (err, client, done) {
@@ -22,6 +22,17 @@ router.get('/adminusers', function(req, res) {
             console.log(result.rows);
             done();
         });
+    });
+});
+
+router.post('/activateuser/:st/:id', function(req, res) {
+    pg.connect(conString, function (err, client, done) {
+        if (err) {
+            return console.error('error fetching client from pool', err);
+        }
+        client.query('UPDATE public.user SET account_status=$1 WHERE user_id=$2', [req.params.st, req.params.id]);
+        res.send(req.params.st);
+        done();
     });
 });
 
